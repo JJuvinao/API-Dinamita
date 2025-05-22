24b6b7b1c2dffa2a096013e861099e6ef86f0fcd
niño <jjuvinao@unicesar.edu.co>
2025-05-21 21:12:19 -0500

    controles eventos

--- API Dinamita/Migrations/20250522020930_cambios.Designer.cs
+++ API Dinamita/Migrations/20250522020930_cambios.Designer.cs
@@ -4,6 +4,7 @@ using API_Dinamita.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API_Dinamita.Migrations
 {
     [DbContext(typeof(ContextDB))]
-    partial class ContextDBModelSnapshot : ModelSnapshot
+    [Migration("20250522020930_cambios")]
+    partial class cambios
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
